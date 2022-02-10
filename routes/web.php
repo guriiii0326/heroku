8d7bd0d5c5ce72f9fa6048f2372873fbce993604
T-ooguri <bmff0326@outlook.jp>
2022-02-10 05:57:12 +0000

    5 commit

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,7 @@ Route::group(['prefix' => 'admin','middleware' => 'auth'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit');
     Route::post('news/edit', 'Admin\NewsController@update');
     Route::get('news/delete', 'Admin\NewsController@delete');
-
 });
-
-//Route::get('XXX', 'AAAController@bbb');
-
 
 Auth::routes();
 
